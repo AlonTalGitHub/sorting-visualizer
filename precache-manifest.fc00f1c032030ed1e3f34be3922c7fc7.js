022c0ad663c0dfee4dc26557d0c5136d749ffd7a
Alon Tal <alontal23@gmail.com>
2020-04-27 16:11:59 +0300

    Updates

---
+++ precache-manifest.fc00f1c032030ed1e3f34be3922c7fc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39eec0cd0df33b69d381b734f3f1896b",
+    "url": "/sorting-visualizer/index.html"
+  },
+  {
+    "revision": "c2e49bcd2c5bdf6dc3ff",
+    "url": "/sorting-visualizer/static/css/main.50814c4b.chunk.css"
+  },
+  {
+    "revision": "1d5b785f26519745b3ab",
+    "url": "/sorting-visualizer/static/js/2.3ece9722.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sorting-visualizer/static/js/2.3ece9722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e49bcd2c5bdf6dc3ff",
+    "url": "/sorting-visualizer/static/js/main.1b93bc14.chunk.js"
+  },
+  {
+    "revision": "0a7a0a9d9134d98e981a",
+    "url": "/sorting-visualizer/static/js/runtime-main.2c0244e7.js"
+  }
+]);
