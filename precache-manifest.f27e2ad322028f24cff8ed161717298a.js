e50ffa1ea9f97e58042c81dc98a60cf05377f079
Alon Tal <alontal23@gmail.com>
2020-04-28 19:47:41 +0300

    Updates

--- precache-manifest.f27e2ad322028f24cff8ed161717298a.js
+++ precache-manifest.f27e2ad322028f24cff8ed161717298a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c64babd20c5b3530b6230c163551a2b5",
+    "revision": "350b2c291d1777ba868b91548ffc1b81",
     "url": "/sorting-visualizer/index.html"
   },
   {
-    "revision": "4b45ab2421f8b323b88d",
-    "url": "/sorting-visualizer/static/css/main.2b10aff6.chunk.css"
+    "revision": "69c6fe5c0f4fb395a610",
+    "url": "/sorting-visualizer/static/css/main.06ad70ca.chunk.css"
   },
   {
     "revision": "1d5b785f26519745b3ab",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/sorting-visualizer/static/js/2.3ece9722.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4b45ab2421f8b323b88d",
+    "revision": "69c6fe5c0f4fb395a610",
     "url": "/sorting-visualizer/static/js/main.e066d13b.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.44c4558077c8861c443bd7185bbf7afe.js"
+  "/sorting-visualizer/precache-manifest.f27e2ad322028f24cff8ed161717298a.js"
 );
 
 self.addEventListener('message', (event) => {
